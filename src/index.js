af922d6afe10923c7d5ce54da49dd46faa6f8c6b
naat-taan <mateus.natan7@gmail.com>
2025-06-19 00:51:55 -0300

    Implementação do Vite

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/App';
-import './src/styles.css'; // Importar estilos globais
+import App from './App';
+import './styles.css'; // Importar estilos globais
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
